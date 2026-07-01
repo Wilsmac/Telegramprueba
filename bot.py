b1937ffa13521795d8ccbdd5cb4de3131b97b2ef
Wilmer ofc <134551880+Wilsmac@users.noreply.github.com>
2026-06-30 19:06:09 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ filters
 )
 
 from handlers.messages import mensaje
-from handlers.commands import start, help, reset, ping, config
+from handlers.commands import start, help, reset, ping, config, stats, about
 from config import TOKEN
 from handlers.images import imagen
 
@@ -17,6 +17,8 @@ app.add_handler(CommandHandler("help",help))
 app.add_handler(CommandHandler("reset",reset))
 app.add_handler(CommandHandler("ping", ping))
 app.add_handler(CommandHandler("config", config))
+app.add_handler(CommandHandler("stats", stats))
+app.add_handler(CommandHandler("about", about))
 
 app.add_handler(
     MessageHandler(
